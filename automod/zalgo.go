b8b1cdc350501ae7555790ef2e2d6cbc74c14ed2
Lewis Crichton <lewistehminerz@outlook.com>
2021-04-17 22:48:04 +0100

    fix zalgo

--- automod/zalgo.go
+++ automod/zalgo.go
@@ -2,16 +2,11 @@ package automod
 
 import "regexp"
 
-const ZALGOREGEX = `(?gu)[\p{Mn}\p{Me}]+`
+var regex = regexp.MustCompile(`[\p{Mn}\p{Me}]+`)
 
 // Return true if all is okay, return false if not.
 func ZalgoCheck(m string) bool {
-	r := regexp.MustCompile(ZALGOREGEX)
+	match := regex.MatchString(m)
 
-	match := r.MatchString(m)
-
-	if match {
-		return false
-	}
-	return true
+	return !match
 }
