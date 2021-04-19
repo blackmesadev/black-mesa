c5b6279c1f361f73dac7425abc5d7c1a09aedda0
Lewis Crichton <lewistehminerz@outlook.com>
2021-04-19 20:55:46 +0100

    stop gopls from whining about this

--- automod/messages.go
+++ automod/messages.go
@@ -6,8 +6,5 @@ import (
 
 func RemoveMessage(s *discordgo.Session, m *discordgo.Message) bool {
 	err := s.ChannelMessageDelete(m.ChannelID, m.ID)
-	if err != nil {
-		return false
-	}
-	return true
+	return err == nil
 }
