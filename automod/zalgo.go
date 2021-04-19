9f3a82379696d3430c84e53e1d472b841716cf4f
Tyler Thompson <tyler.thompson@cirencester-friendly.co.uk>
2021-04-19 04:14:10 +0100

    regex optimisation

--- automod/zalgo.go
+++ automod/zalgo.go
@@ -2,11 +2,11 @@ package automod
 
 import "regexp"
 
-var regex = regexp.MustCompile(`[\p{Mn}\p{Me}]+`)
+var zalgoRegex = regexp.MustCompile(`[\p{Mn}\p{Me}]+`)
 
 // Return true if all is okay, return false if not.
 func ZalgoCheck(m string) bool {
-	match := regex.MatchString(m)
+	match := zalgoRegex.MatchString(m)
 
 	return !match
 }
