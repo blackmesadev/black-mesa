b5308a853d1ca0fd0ae9541bea0dfbd09946b4bf
Tyler Thompson <tyler.thompson@cirencester-friendly.co.uk>
2021-04-20 20:29:13 +0100

    added week (whoops aha) and added improved parsing (that works now :O )

--- moderation/moderation.go
+++ moderation/moderation.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var snowflakeRegex = regexp.MustCompile(`([0-9]{17,18})`)
+
+var numberRegex = regexp.MustCompile(`[0-9]*[.]?[0-9]+`)
 
 // seconds to regex for the string of it, makes iteration easier as you can use k as the multiplier for v
 var timeRegex = map[int64]*regexp.Regexp{
@@ -15,22 +18,34 @@ var timeRegex = map[int64]*regexp.Regexp{
 	60:       regexp.MustCompile(`(\d+)m`),
 	3600:     regexp.MustCompile(`(\d+)h`),
 	86400:    regexp.MustCompile(`(\d+)d`),
+	604800:   regexp.MustCompile(`(\d+)w`),
 	2628000:  regexp.MustCompile(`(\d+)mo`),
 	31536000: regexp.MustCompile(`(\d+)y`),
 }
 
 func parseCommand(cmd string) ([]string, int64, string) {
+	var reason string
 
 	idList := snowflakeRegex.FindAllString(cmd, -1)
-	duration, lastRegex := parseTime(cmd)
 
-	reasonSearch := lastRegex.FindAllString(cmd, -1)
+	params := snowflakeRegex.Split(cmd, -1)
 
-	reason := reasonSearch[len(reasonSearch)-1]
+	if params[len(params)-1][:1] == ">" {
+		reason = params[len(params)-1][1:]
+	} else {
+		reason = params[len(params)-1]
+	}
+
+	durationStr := strings.Fields(reason)[0]
+	duration := parseTime(durationStr)
+
+	reason = strings.ReplaceAll(reason, durationStr, "")
+
+	reason = strings.TrimSpace(reason)
 
 	fmt.Println(idList)
 	fmt.Println(duration)
-	fmt.Println(reasonSearch)
+	fmt.Println(params)
 	fmt.Println(reason)
 
 	return idList, duration, reason
@@ -40,8 +55,7 @@ func parseCommand(cmd string) ([]string, int64, string) {
 // also returns the last regex that works so that we can split the command with an already compiled
 // regex which will give us the reason at the end! (if it doesnt its because the reason consisted of someone
 // slamming their head against their keyboard for the reason and at that point i dont give a fuck)
-func parseTime(strTime string) (int64, *regexp.Regexp) {
-	var lastRegex *regexp.Regexp
+func parseTime(strTime string) int64 {
 	var unixTime int64
 
 	unixTime = time.Now().Unix()
@@ -49,15 +63,15 @@ func parseTime(strTime string) (int64, *regexp.Regexp) {
 	for multiplier, regex := range timeRegex {
 		timeValStrSlice := regex.FindAllString(strTime, -1)
 		if timeValStrSlice != nil {
-			timeVal, err := strconv.ParseInt(timeValStrSlice[0], 10, 32) // will be cast to uint32 so needs to be int32 at heart in an int64 body
+			timeVal, err := strconv.ParseInt(numberRegex.FindAllString(timeValStrSlice[0], 1)[0], 10, 32) // will be cast to uint32 so needs to be int32 at heart in an int64 body
 			if err != nil {
-				fmt.Println(err)
-				unixTime = 4294967295
+				if strings.Contains(err.Error(), "strconv.ParseInt: parsing") {
+					return 0
+				}
 			}
 			unixTime += timeVal * multiplier
-			lastRegex = regex
 		}
 	}
 
-	return unixTime, lastRegex
+	return unixTime
 }
