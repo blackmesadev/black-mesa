86bca97ff51b84cb51ab84cd6ba19bac04057fe6
Tyler Thompson <tyler.thompson@cirencester-friendly.co.uk>
2021-04-20 20:28:19 +0100

    actually parses now woo

--- moderation/kick.go
+++ moderation/kick.go
@@ -2,30 +2,47 @@ package moderation
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/blackmesadev/discordgo"
 )
 
 func KickCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context) {
+	var reason string
+
 	start := time.Now()
 
 	idList := snowflakeRegex.FindAllString(m.Content, -1)
 
+	reasonSearch := snowflakeRegex.Split(m.Content, -1)
+
+	if reasonSearch[len(reasonSearch)-1][:1] == ">" {
+		reason = reasonSearch[len(reasonSearch)-1][1:]
+	} else {
+		reason = reasonSearch[len(reasonSearch)-1]
+	}
+
+	reason = strings.TrimSpace(reason)
+
 	msg := "Successfully removed "
 
-	unableBan := make([]string, 0)
+	unableKick := make([]string, 0)
 	for _, id := range idList {
-		err := s.GuildMemberDelete(m.GuildID, id)
+		err := s.GuildMemberDeleteWithReason(m.GuildID, id, reason)
 		if err != nil {
-			unableBan = append(unableBan, id)
+			unableKick = append(unableKick, id)
 		} else {
-			msg += fmt.Sprintf("<@%v>", id)
+			msg += fmt.Sprintf("<@%v> ", id)
 		}
 	}
 
-	if len(unableBan) != 0 {
-		msg += fmt.Sprintf("\nCould not remove %v", unableBan)
+	if len(reason) != 0 {
+		msg += fmt.Sprintf("for reason `%v` ", reason)
+	}
+
+	if len(unableKick) != 0 {
+		msg += fmt.Sprintf("\nCould not remove %v", unableKick)
 	}
 
 	s.ChannelMessageSend(m.ChannelID, msg)
