4e9d477d274f7ddcf95d376acd2eca116bd26d22
Lewis Crichton <lewistehminerz@outlook.com>
2021-04-28 21:51:08 +0100

    Whoops

--- config/cmds_makeMute.go
+++ config/cmds_makeMute.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MakeMuteCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
-	if !config.CheckPermission(s, m.GuildID, m.Author.ID, "admin") {
+	if !CheckPermission(s, m.GuildID, m.Author.ID, "admin") {
 		s.ChannelMessageSend(m.ChannelID, "<:mesaCross:832350526414127195> You do not have permission for that.")
 		return
 	}
