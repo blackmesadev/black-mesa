cc3349c77c00935fd493b9d65296b0f4f3ef1d44
Tyler Thompson <yoitscore@gmail.com>
2021-07-17 06:55:52 +0100

    move all regex to misc

---
+++ misc/regex.go
@@ -0,0 +1,22 @@
+package misc
+
+import "regexp"
+
+var UserIdRegex = regexp.MustCompile(`^(?:<@!?)?(\d+)>?$`)
+
+var RoleIdRegex = regexp.MustCompile(`^(?:<@&!?)?(\d+)>?$`)
+
+var SnowflakeRegex = regexp.MustCompile(`([0-9]{17,18})`)
+
+var NumberRegex = regexp.MustCompile(`[0-9]*[.]?[0-9]+`)
+
+// seconds to regex for the string of it, makes iteration easier as you can use k as the multiplier for v
+var TimeRegex = map[int64]*regexp.Regexp{
+	1:        regexp.MustCompile(`(\d+)s`),
+	60:       regexp.MustCompile(`(\d+)m`),
+	3600:     regexp.MustCompile(`(\d+)h`),
+	86400:    regexp.MustCompile(`(\d+)d`),
+	604800:   regexp.MustCompile(`(\d+)w`),
+	2628000:  regexp.MustCompile(`(\d+)mo`),
+	31536000: regexp.MustCompile(`(\d+)y`),
+}
