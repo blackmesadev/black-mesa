f94d7a8ec4e8e0a33f63af3914a558124d13cf13
Tyler Thompson <yoitscore@gmail.com>
2021-07-20 07:53:16 +0100

    debug instance with more detail

--- config/botInfo.go
+++ config/botInfo.go
@@ -1,5 +1,5 @@
 package config
 
 func GetVersion() string {
-	return "0.2.8"
+	return "0.2.9"
 }
--- discord/OnReady.go
+++ discord/OnReady.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/blackmesadev/black-mesa/util"
 	"github.com/blackmesadev/discordgo"
 )
 
 func (bot *Bot) OnReady(s *discordgo.Session, r *discordgo.Ready) {
+	if util.IsDevInstance(s) {
+		s.Debug = true
+	}
 	fmt.Printf("Black Mesa ready at %v\nGuilds: %v\nRunning on account ID %v (%v).",
 		time.Now().UTC(), len(r.Guilds), r.User.ID, r.User.String())
 }
