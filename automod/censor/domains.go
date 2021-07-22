377b41a4b91c38e16eaf92a1ec961175aea8eb5d
Tyler Thompson <yoitscore@gmail.com>
2021-07-22 18:37:13 +0100

    this should say domains lol

--- automod/censor/domains.go
+++ automod/censor/domains.go
@@ -18,12 +18,12 @@ func DomainsWhitelistCheck(m string, whitelist *[]string) (bool, string) {
 
 	fmt.Println(domains)
 
-	for _, invite := range domains {
+	for _, domain := range domains {
 		for _, whitelistedDomain := range *whitelist {
-			if invite == whitelistedDomain {
+			if domain == whitelistedDomain {
 				ok = true
 			} else {
-				return false, invite
+				return false, domain
 			}
 		}
 	}
@@ -33,16 +33,16 @@ func DomainsWhitelistCheck(m string, whitelist *[]string) (bool, string) {
 
 func DomainsBlacklistCheck(m string, blacklist *[]string) (bool, string) {
 	ok := true
-	invites := domainsRegex.FindAllString(m, -1)
+	domains := domainsRegex.FindAllString(m, -1)
 
-	if len(invites) == 0 {
+	if len(domains) == 0 {
 		return true, ""
 	}
 
-	for _, invite := range invites {
+	for _, domain := range domains {
 		for _, blacklistedDomain := range *blacklist {
-			if invite == blacklistedDomain {
-				return false, invite
+			if domain == blacklistedDomain {
+				return false, domain
 			} else {
 				ok = true
 			}
