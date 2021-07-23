159a1d42615d255665c1301bbffbcd1973d25446
Tyler Thompson <yoitscore@gmail.com>
2021-07-23 21:25:34 +0100

    calc stats every 15 seconds and store in redis

---
+++ util/stats.go
@@ -0,0 +1,54 @@
+package util
+
+import (
+	"time"
+
+	bmRedis "github.com/blackmesadev/black-mesa/redis"
+	"github.com/blackmesadev/discordgo"
+	"github.com/go-redis/redis/v8"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+var r *redis.Client
+
+func CalcStats(s *discordgo.Session) {
+	r = bmRedis.GetRedis()
+
+	for {
+
+		time.Sleep(15 * time.Second)
+
+		// Sys Stats
+		v, _ := mem.VirtualMemory()
+
+		usedMem := v.UsedPercent
+
+		c, _ := cpu.Percent(time.Duration(1*time.Second), true)
+
+		var usedCpu float64
+
+		for _, i := range c {
+			usedCpu += i
+		}
+
+		usedCpu /= float64(len(c))
+
+		// Discord stats
+
+		var memberCount int
+
+		for _, i := range s.State.Guilds {
+			memberCount += i.MemberCount
+		}
+
+		// Send everything to Redis
+
+		r.Set(r.Context(), "usedMem", usedMem, time.Minute)
+
+		r.Set(r.Context(), "usedCpu", usedCpu, time.Minute)
+
+		r.Set(r.Context(), "memberCount", memberCount, time.Minute)
+	}
+
+}
