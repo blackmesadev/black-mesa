1480283aeeabc040d4b72508f827e7dce104a149
Tyler Thompson <yoitscore@gmail.com>
2021-07-26 15:03:29 +0100

    use consts with permission string + rename removeInfraction to removeAction

--- moderation/removeAction.go
+++ moderation/removeAction.go
@@ -12,8 +12,8 @@ import (
 	"github.com/blackmesadev/discordgo"
 )
 
-func RemoveInfractionCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
-	if !config.CheckPermission(s, m.GuildID, m.Author.ID, "moderation.remove") {
+func RemoveActionCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+	if !config.CheckPermission(s, m.GuildID, m.Author.ID, PERMISSION_REMOVEACTION) {
 		s.ChannelMessageSend(m.ChannelID, "<:mesaCross:832350526414127195> You do not have permission for that.")
 		return
 	}
