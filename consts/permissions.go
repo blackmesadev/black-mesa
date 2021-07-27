fbeed130e06da3c3f38fba3b5ec33e102a4e1cf3
Tyler Thompson <yoitscore@gmail.com>
2021-07-27 20:58:42 +0100

    move permissions to consts/permissions.go

---
+++ consts/permissions.go
@@ -0,0 +1,14 @@
+package consts
+
+const (
+	PERMISSION_BAN          = "moderation.ban"
+	PERMISSION_KICK         = "moderation.kick"
+	PERMISSION_MUTE         = "moderation.mute"
+	PERMISSION_PURGE        = "moderation.purge"
+	PERMISSION_REMOVEACTION = "moderation.remove"
+	PERMISSION_SEARCH       = "moderation.search"
+	PERMISSION_SOFTBAN      = "moderation.softban"
+	PERMISSION_STRIKE       = "moderation.strike"
+	PERMISSION_UNBAN        = "moderation.unban"
+	PERMISSION_UNMUTE       = "moderation.unmute"
+)
--- moderation/unmute.go
+++ moderation/unmute.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/blackmesadev/black-mesa/config"
+	"github.com/blackmesadev/black-mesa/consts"
 	"github.com/blackmesadev/black-mesa/logging"
 	"github.com/blackmesadev/black-mesa/misc"
 	"github.com/blackmesadev/black-mesa/util"
@@ -13,7 +14,7 @@ import (
 )
 
 func UnmuteCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
-	if !config.CheckPermission(s, m.GuildID, m.Author.ID, PERMISSION_UNMUTE) {
+	if !config.CheckPermission(s, m.GuildID, m.Author.ID, consts.PERMISSION_UNMUTE) {
 		s.ChannelMessageSend(m.ChannelID, "<:mesaCross:832350526414127195> You do not have permission for that.")
 		return
 	}
