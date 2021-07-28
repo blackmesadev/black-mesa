75a376d6dd0d48d144af49744de73a0e2de7aa70
Tyler Thompson <yoitscore@gmail.com>
2021-07-28 22:07:37 +0100

    add purge types

---
+++ consts/purge.go
@@ -0,0 +1,13 @@
+package consts
+
+type PURGE string
+
+const (
+	PURGE_ALL          = "all"
+	PURGE_ATTACHEMENTS = "attachments"
+	PURGE_BOT          = "bots"
+	PURGE_IMAGE        = "images"
+	PURGE_STRING       = "string"
+	PURGE_USER         = "users"
+	PURGE_VIDEO        = "videos"
+)
