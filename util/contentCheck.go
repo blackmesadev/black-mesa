94e436b2f6d2d7511ad947f7246635b1b88e90f6
Lewis Crichton <lewistehminerz@outlook.com>
2021-07-31 21:17:32 +0100

    better content check

--- util/contentCheck.go
+++ util/contentCheck.go
@@ -3,10 +3,28 @@ package util
 import (
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/blackmesadev/discordgo"
 )
+
+var domainsRegex = regexp.MustCompile(`(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)`)
+
+func checkHeaders(url string, lookingFor string) bool {
+	resp, err := http.Head(url)
+	if err != nil {
+		log.Println(err)
+	} else {
+		header := resp.Header.Get("Content-Type")
+		if strings.HasPrefix(header, lookingFor) {
+			return true
+		}
+	}
+
+	return false
+}
 
 func CheckForImage(m *discordgo.Message) bool {
 
@@ -21,17 +39,58 @@ func CheckForImage(m *discordgo.Message) bool {
 	if len(m.Attachments) > 0 {
 		for _, attachment := range m.Attachments {
 			// We only need headers so dont waste our time downloaing whole file
-			resp, err := http.Head(attachment.URL)
-			if err != nil {
-				log.Println(err)
-			} else {
-				header := resp.Header.Get("Content-Type")
-				if strings.HasPrefix(header, "image") {
-					return true
-				}
+			result := checkHeaders(attachment.URL, "image")
+			if result {
+				return true
 			}
 		}
 	}
+
+	// worst case scenario, download any web pages in the message:
+	domains := domainsRegex.FindAllString(m.Content, -1)
+
+	for _, domain := range domains {
+		resp, err := http.Get(domain)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		foundInMeta := false
+		doc.Find("meta").Each(func (i int, s *goquery.Selection) {
+			tagName, exists := s.Attr("name")
+
+			if exists {
+				if tagName == "twitter:image" || tagName == "og:image" {
+					content, exists := s.Attr("value")
+
+					if !exists {
+						content, exists = s.Attr("content")
+						if !exists {
+							log.Println("Meta tag without value?")
+							return
+						}
+					}
+
+					result := checkHeaders(content, "image")
+					if result {
+						foundInMeta = true
+					}
+				}
+			}
+		})
+
+		if foundInMeta {
+			return true
+		}
+	}
+
 	return false
 }
 
@@ -47,15 +106,9 @@ func CheckForVideo(m *discordgo.Message) bool {
 
 	if len(m.Attachments) > 0 {
 		for _, attachment := range m.Attachments {
-			// We only need headers so dont waste our time downloaing whole file
-			resp, err := http.Head(attachment.URL)
-			if err != nil {
-				log.Println(err)
-			} else {
-				header := resp.Header.Get("Content-Type")
-				if strings.HasPrefix(header, "video") {
-					return true
-				}
+			result := checkHeaders(attachment.URL, "video")
+			if result {
+				return true
 			}
 		}
 	}
