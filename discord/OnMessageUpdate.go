40efe50138e86fdaa21f54b466b74ca69876a278
damaredayo <yoitscore@gmail.com>
2021-08-02 21:39:42 +0100

    ignore bots on edit too

--- discord/OnMessageUpdate.go
+++ discord/OnMessageUpdate.go
@@ -7,6 +7,9 @@ import (
 )
 
 func (bot *Bot) OnMessageUpdate(s *discordgo.Session, m *discordgo.MessageUpdate) {
+	if m.Author.Bot {
+		return
+	} // just ignore all bot messages, good bots don't need to be moderated by us
 	if m.BeforeUpdate != nil && m.Author != nil {
 		logging.LogMessageUpdate(s, m.Message, m.BeforeUpdate.Content)
 		automod.Process(s, m.Message)
