ce62a3f6a8f0b51248e5ea47c1752fe4b7a0d460
Tyler Thompson <yoitscore@gmail.com>
2021-08-09 04:20:32 +0100

    add status

--- discord/OnReady.go
+++ discord/OnReady.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/blackmesadev/black-mesa/config"
+	"github.com/blackmesadev/black-mesa/info"
 	"github.com/blackmesadev/black-mesa/music"
 	"github.com/blackmesadev/black-mesa/util"
 	"github.com/blackmesadev/discordgo"
@@ -14,6 +15,21 @@ func (bot *Bot) OnReady(s *discordgo.Session, r *discordgo.Ready) {
 	if util.IsDevInstance(s) {
 		s.Debug = true
 	}
+
+	idle := 0
+	s.UpdateStatusComplex(discordgo.UpdateStatusData{
+		IdleSince: &idle,
+		Activities: []*discordgo.Activity{
+			{
+				Name:      fmt.Sprintf("Black Mesa %v", info.VERSION),
+				Type:      discordgo.ActivityTypeCustom,
+				URL:       info.WEBSITE,
+				CreatedAt: time.Now(),
+			},
+		},
+		AFK:    false,
+		Status: fmt.Sprintf("Black Mesa %v", info.VERSION),
+	})
 
 	music.LavalinkInit(r, config.LoadLavalinkConfig())
 
