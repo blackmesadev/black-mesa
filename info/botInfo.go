845ba8aedccf880a35dcb16faa3e2455a9750d93
Tyler Thompson <yoitscore@gmail.com>
2021-10-03 14:40:54 +0100

    4 characters, not 2

--- info/botInfo.go
+++ info/botInfo.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	VERSION = "0.12.5"
+	VERSION = "0.12.6"
 )
 
 var r *redis.Client
--- util/filteredCommand.go
+++ util/filteredCommand.go
@@ -13,13 +13,13 @@ func FilteredCommand(input string) (output string) {
 	if strings.Contains(input, consts.CENSOR_STRINGS) {
 		blockedString = strings.TrimSpace(strings.TrimPrefix(input, consts.CENSOR_STRINGS))
 		outputLength := len(blockedString)
-		output = fmt.Sprintf("%v `(%c%v%c)`", consts.CENSOR_STRINGS, blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
+		output = fmt.Sprintf("%v `(%c%v%c)`", consts.CENSOR_STRINGS, blockedString[1], strings.Repeat("*", outputLength-4), blockedString[outputLength-2])
 	}
 
 	if strings.Contains(input, consts.CENSOR_SUBSTRINGS) {
 		blockedString = strings.TrimSpace(strings.TrimPrefix(input, consts.CENSOR_SUBSTRINGS))
 		outputLength := len(blockedString)
-		output = fmt.Sprintf("%v `(%c%v%c)`", consts.CENSOR_SUBSTRINGS, blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
+		output = fmt.Sprintf("%v `(%c%v%c)`", consts.CENSOR_SUBSTRINGS, blockedString[1], strings.Repeat("*", outputLength-4), blockedString[outputLength-2])
 	}
 
 	return
