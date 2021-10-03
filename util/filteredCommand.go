caf19131c31368c20ffbf8734cd07f3751732a17
Tyler Thompson <yoitscore@gmail.com>
2021-10-03 13:13:49 +0100

    no need to use labels + use %c in sprintf

--- util/filteredCommand.go
+++ util/filteredCommand.go
@@ -10,16 +10,13 @@ func FilteredCommand(input string) (output string) {
 
 	if strings.HasPrefix(input, "Censor->BlockedString") {
 		blockedString = strings.TrimPrefix(input, "Censor->BlockedString ")
-		goto makeOutput
 	}
 
 	if strings.HasPrefix(input, "Censor->BlockedSubString") {
 		blockedString = strings.TrimPrefix(input, "Censor->BlockedSubString ")
-		goto makeOutput
 	}
 
-makeOutput:
 	outputLength := len(blockedString)
-	output = fmt.Sprintf("*%v%v%v*", blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
+	output = fmt.Sprintf("*%c%v%c*", blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
 	return
 }
