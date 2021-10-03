f7ef1d0582a859fe64497ea3c32a5cd5ba3db04d
Tyler Thompson <yoitscore@gmail.com>
2021-10-03 14:26:30 +0100

    update filteredcommand to use consts

--- util/filteredCommand.go
+++ util/filteredCommand.go
@@ -3,20 +3,24 @@ package util
 import (
 	"fmt"
 	"strings"
+
+	"github.com/blackmesadev/black-mesa/consts"
 )
 
 func FilteredCommand(input string) (output string) {
 	var blockedString string
 
-	if strings.HasPrefix(input, "Censor->BlockedString") {
-		blockedString = strings.TrimPrefix(input, "Censor->BlockedString ")
+	if strings.Contains(input, consts.CENSOR_STRINGS) {
+		blockedString = strings.TrimSpace(strings.TrimPrefix(input, consts.CENSOR_STRINGS))
+		outputLength := len(blockedString)
+		output = fmt.Sprintf("%v (%c%v%c)", consts.CENSOR_STRINGS, blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
 	}
 
-	if strings.HasPrefix(input, "Censor->BlockedSubString") {
-		blockedString = strings.TrimPrefix(input, "Censor->BlockedSubString ")
+	if strings.Contains(input, consts.CENSOR_SUBSTRINGS) {
+		blockedString = strings.TrimSpace(strings.TrimPrefix(input, consts.CENSOR_SUBSTRINGS))
+		outputLength := len(blockedString)
+		output = fmt.Sprintf("%v (%c%v%c)", consts.CENSOR_SUBSTRINGS, blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
 	}
 
-	outputLength := len(blockedString)
-	output = fmt.Sprintf("*%c%v%c*", blockedString[1], strings.Repeat("*", outputLength-2), blockedString[outputLength-2])
 	return
 }
