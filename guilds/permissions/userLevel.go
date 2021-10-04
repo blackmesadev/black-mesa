fd638d01e9008116ff1552e4fba0170fafbc2676
Tyler Thompson <yoitscore@gmail.com>
2021-10-04 03:49:28 +0100

    add get user level cmd

---
+++ guilds/permissions/userLevel.go
@@ -0,0 +1,47 @@
+package permissions
+
+import (
+	"fmt"
+
+	"github.com/blackmesadev/black-mesa/config"
+	"github.com/blackmesadev/black-mesa/consts"
+	"github.com/blackmesadev/black-mesa/util"
+	"github.com/blackmesadev/discordgo"
+)
+
+func GetUserLevelCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+	idList := util.SnowflakeRegex.FindAllString(m.Content, -1)
+
+	if len(idList) == 0 {
+		idList = append(idList, m.Author.ID)
+	}
+
+	if !config.CheckPermission(s, m.GuildID, m.Author.ID, consts.PERMISSION_VIEWPERMS) && idList[0] != m.Author.ID {
+		s.ChannelMessageSend(m.ChannelID, "<:mesaCross:832350526414127195> You do not have permission for that.")
+		return
+	}
+
+	if len(idList) > 10 {
+		s.ChannelMessageSend(m.ChannelID, "<:mesaCommand:832350527131746344> `getuserlevel` takes a maximum of 10 `<target:user[]>` parameters.")
+		return
+	}
+
+	msg := "```\nPermission Levels:\n"
+
+	for _, id := range idList {
+		var memberName string
+
+		lvl := config.GetLevel(s, m.GuildID, id)
+
+		member, err := s.State.Member(m.GuildID, id)
+		if err != nil {
+			memberName = id
+		} else {
+			memberName = member.User.String()
+		}
+		msg = fmt.Sprintf("%v%v:`%d`\n", msg, memberName, lvl)
+	}
+	msg += "```"
+
+	s.ChannelMessageSend(m.ChannelID, msg)
+}
