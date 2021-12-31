8d3396d263d3682d7211019714e0f35fc015b6b5
Tyler Thompson <tyler.thompson@infosum.com>
2021-12-31 17:07:13 +0000

    updated search to support GetPunishmentByUUID

--- moderation/search.go
+++ moderation/search.go
@@ -10,6 +10,7 @@ import (
 	"github.com/blackmesadev/black-mesa/config"
 	"github.com/blackmesadev/black-mesa/consts"
 	"github.com/blackmesadev/black-mesa/info"
+	"github.com/blackmesadev/black-mesa/logging"
 	"github.com/blackmesadev/black-mesa/util"
 	"github.com/blackmesadev/discordgo"
 )
@@ -17,7 +18,9 @@ import (
 func SearchCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
 	idList := util.SnowflakeRegex.FindAllString(m.Content, -1)
 
-	if len(idList) == 0 {
+	uuidList := util.UuidRegex.FindAllString(m.Content, -1)
+
+	if len(idList) == 0 && len(uuidList) == 0 {
 		idList = append(idList, m.Author.ID)
 	}
 
@@ -26,10 +29,30 @@ func SearchCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Contex
 		return
 	}
 
-	if len(idList) > 1 {
-		s.ChannelMessageSend(m.ChannelID, "<:mesaCommand:832350527131746344> `search` takes 1 `<target:user[]>` parameter.")
+	if len(idList) > 1 || len(uuidList) > 1 {
+		s.ChannelMessageSend(m.ChannelID, "<:mesaCommand:832350527131746344> `search` takes 1 `[target:user]` or `[infraction:uuid]` parameter.")
 		return
 	}
+
+	if len(idList) == 1 && len(uuidList) == 1 {
+		s.ChannelMessageSend(m.ChannelID, "<:mesaCommand:832350527131746344> `search` takes 1 `[target:user]` or `[infraction:uuid]` parameter.")
+		return
+	}
+
+	var err error
+	if len(idList) == 1 {
+		_, err = SearchByUser(s, m, idList)
+	} else {
+		_, err = SearchByUUID(s, m, uuidList)
+	}
+
+	if err != nil {
+		logging.LogError(s, m.GuildID, "", "SearchCmd", err)
+	}
+
+}
+
+func SearchByUser(s *discordgo.Session, m *discordgo.Message, idList []string) (*discordgo.Message, error) {
 
 	punishmentList, err := config.GetPunishments(m.GuildID, idList[0])
 	if err != nil {
@@ -52,8 +75,9 @@ func SearchCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Contex
 			issuer = punishment.Issuer
 		}
 		field := &discordgo.MessageEmbedField{
-			Name:   strings.Title(punishment.Type),
-			Value:  fmt.Sprintf("**Reason:** %v\n**Issued By:** %v\n**UUID:** %v\n**Expiring:** %v", util.FilteredCommand(punishment.Reason), issuer, punishment.UUID, time.Unix(punishment.Expires, 0)),
+			Name: strings.Title(punishment.Type),
+			Value: fmt.Sprintf("**Reason:** %v\n**Issued By:** %v\n**UUID:** %v\n**Expiring:** %v",
+				util.FilteredCommand(punishment.Reason), issuer, punishment.UUID, time.Unix(punishment.Expires, 0)),
 			Inline: true,
 		}
 		embedFields = append(embedFields, field)
@@ -78,5 +102,48 @@ func SearchCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Contex
 		Color:  0, // Black int value
 		Fields: embedFields,
 	}
-	_, err = s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	return s.ChannelMessageSendEmbed(m.ChannelID, embed)
+
+}
+
+func SearchByUUID(s *discordgo.Session, m *discordgo.Message, uuidList []string) (*discordgo.Message, error) {
+
+	punishment, err := config.GetPunishmentByUUID(m.GuildID, uuidList[0])
+	if err != nil {
+		logging.LogError(s, m.GuildID, "", "SearchByUUID", err)
+	}
+
+	footer := &discordgo.MessageEmbedFooter{
+		Text: fmt.Sprintf("Black Mesa %v by Tyler#0911 & LewisTehMinerz#1337 running on %v", info.VERSION, runtime.Version()),
+	}
+
+	var issuer string
+	if punishment.Issuer != "AutoMod" {
+		user, err := s.User(punishment.Issuer)
+		if err != nil {
+			issuer = punishment.Issuer
+		} else {
+			issuer = user.String()
+		}
+	} else {
+		issuer = punishment.Issuer
+	}
+	embedFields := []*discordgo.MessageEmbedField{
+		{
+			Name: strings.Title(punishment.Type),
+			Value: fmt.Sprintf("**Reason:** %v\n**Issued By:**%v\n**Expiring:** %v",
+				util.FilteredCommand(punishment.Reason), issuer, time.Unix(punishment.Expires, 0)),
+			Inline: true,
+		},
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title:  fmt.Sprintf("Infraction: %v", punishment.UUID),
+		Type:   discordgo.EmbedTypeRich,
+		Footer: footer,
+		Color:  0, // Black int value
+		Fields: embedFields,
+	}
+
+	return s.ChannelMessageSendEmbed(m.ChannelID, embed)
 }
