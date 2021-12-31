0fb956dffe3d1833a843773cb7cd4d8546f9e6ca
Tyler Thompson <tyler.thompson@infosum.com>
2021-12-31 16:59:28 +0000

    updated ping cmd

--- misc/ping.go
+++ misc/ping.go
@@ -11,15 +11,8 @@ func PingCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context,
 	recievedTime := time.Now()
 
 	discordLatency := s.HeartbeatLatency().Milliseconds()
-	pingMsg := fmt.Sprintf("Ping Statistics: Discord:`%vms` ", discordLatency)
-
-	msgTime, err := m.Timestamp.Parse()
-	if err != nil {
-		pingMsg += "Bot:`Unknown`"
-	} else {
-		botLatency := recievedTime.Sub(msgTime)
-		pingMsg += fmt.Sprintf("Bot:`%vms`", botLatency.Milliseconds())
-	}
+	botLatency := recievedTime.Sub(m.Timestamp).Milliseconds()
+	pingMsg := fmt.Sprintf("Ping Statistics: Discord:`%vms` Bot:`%vms`", discordLatency, botLatency)
 
 	s.ChannelMessageSend(m.ChannelID, pingMsg)
 }
