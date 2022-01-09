744c4d804fd0490b20dd98a2bfba9854dee99920
Tyler Thompson <yoitscore@gmail.com>
2022-01-09 02:19:05 +0000

    add IsStaff func

---
+++ config/staff.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"log"
+
+	"github.com/blackmesadev/black-mesa/structs"
+	"github.com/blackmesadev/discordgo"
+)
+
+func IsStaff(s *discordgo.Session, conf *structs.Config, guildid string, userid string) bool {
+	if conf == nil {
+		var err error
+		conf, err = GetConfig(guildid)
+		if err != nil {
+			log.Printf("Failed to get config for %v (%v)\n", guildid, err)
+			return false
+		}
+	}
+
+	lvl := GetLevel(s, guildid, userid)
+
+	if lvl >= conf.Modules.Guild.StaffLevel {
+		return true
+	}
+
+	return false
+}
