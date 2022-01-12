3c3e4152fd9b5e9f9c0f1f138db1d9af87eeffce
Tyler Thompson <yoitscore@gmail.com>
2022-01-12 21:44:21 +0000

    we dont talk about this

--- automod/spam/max_length.go
+++ automod/spam/max_length.go
@@ -3,5 +3,8 @@ package spam
 import "github.com/blackmesadev/discordgo"
 
 func ProcessMaxLength(message *discordgo.Message, limit int64) (bool, int64) {
+	if limit == 0 {
+		return true, 0
+	}
 	return int64(len(message.Content)) <= limit, int64(len(message.Content))
 }
