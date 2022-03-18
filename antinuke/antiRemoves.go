d569a68e914d53dc80fa5261e81f37594998b196
= <=>
2022-03-18 15:06:09 +0000

    update AntiRemoveProcess func

--- antinuke/antiRemoves.go
+++ antinuke/antiRemoves.go
@@ -5,23 +5,24 @@ import (
 	"time"
 
 	bmRedis "github.com/blackmesadev/black-mesa/redis"
+	"github.com/blackmesadev/black-mesa/structs"
 	"github.com/blackmesadev/discordgo"
 	"github.com/go-redis/redis/v8"
 )
 
 var r *redis.Client
 
-func AntiRemoveProcess(audit *discordgo.AuditLogEntry, guildID string, max int64, timeLimit time.Duration) bool {
+func AntiRemoveProcess(s *discordgo.Session, anti structs.AntiNukeThreshold, userID string, guildID string) bool {
 	if r == nil {
 		r = bmRedis.GetRedis()
 	}
 
-	key := fmt.Sprintf("anti:maxRemoves:%v:%v", guildID, audit.UserID)
+	key := fmt.Sprintf("anti:maxRemoves:%v:%v", guildID, userID)
 
 	res, err := r.Get(r.Context(), key).Int64()
 	if err != nil {
 		if err == redis.Nil {
-			r.Set(r.Context(), key, 1, timeLimit)
+			r.Set(r.Context(), key, 1, time.Duration(anti.Interval)*time.Second)
 			res = 1
 		} else {
 			fmt.Println(err)
@@ -31,8 +32,9 @@ func AntiRemoveProcess(audit *discordgo.AuditLogEntry, guildID string, max int64
 
 	r.Incr(r.Context(), key)
 
-	if res > max {
+	if res > anti.Max {
 		return false
+
 	}
 
 	return true
