3f93665c9453a7b11a1bb2e9e2d1f38395e16b9e
Tyler Thompson <yoitscore@gmail.com>
2022-04-16 09:05:07 +0100

    add unimplemented trustworthy commands

---
+++ modules/untrustworthy/commands.go
@@ -0,0 +1,21 @@
+package untrustworthy
+
+import "github.com/blackmesadev/discordgo"
+
+func SubmitUntrustworthyCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
+
+func ApproveUntrustworthyCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
+
+func DenyUntrustworthyCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
+
+func ListUntrustworthyCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
+
+func DeleteUntrustworthyCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
+
+func BlacklistUserFromSubmissionsCmd(s *discordgo.Session, m *discordgo.Message, ctx *discordgo.Context, args []string) {
+}
