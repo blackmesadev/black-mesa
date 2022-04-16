3da2e97de60c5c73adaee874d8d5b6e9bfed253c
Tyler Thompson <yoitscore@gmail.com>
2022-04-16 09:02:07 +0100

    add untrustworthy type consts

---
+++ consts/untrustworthy.go
@@ -0,0 +1,10 @@
+package consts
+
+type UntrustworthyType string
+
+const (
+	UNTRUSTWORTHY_TYPE_UNKNOWN   UntrustworthyType = "unknown"
+	UNTRUSTWORTHY_TYPE_IPGRABBER UntrustworthyType = "ipgrabber"
+	UNTRUSTWORTHY_TYPE_URL       UntrustworthyType = "url"
+	UNTRUSTWORTHY_TYPE_USER      UntrustworthyType = "user"
+)
