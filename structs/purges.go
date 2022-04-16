d8bd94ad9c191e7d1124f234fbca5e22fabcfcd4
Tyler Thompson <yoitscore@gmail.com>
2022-04-16 09:01:48 +0100

    add json/bson struct tags to purge struct

--- structs/purges.go
+++ structs/purges.go
@@ -3,9 +3,9 @@ package structs
 import "github.com/blackmesadev/discordgo"
 
 type PurgeStruct struct {
-	Messages  []*discordgo.Message
-	GuildID   string
-	ChannelID string
-	IssuerID  string
-	UUID      string
+	Messages  []*discordgo.Message `json:"messages,omitempty" bson:"messages,omitempty"`
+	GuildID   string               `json:"guildID,omitempty" bson:"guildID,omitempty"`
+	ChannelID string               `json:"channelID,omitempty" bson:"channelID,omitempty"`
+	IssuerID  string               `json:"issuerID,omitempty" bson:"issuerID,omitempty"`
+	UUID      string               `json:"uuid,omitempty" bson:"uuid,omitempty"`
 }
